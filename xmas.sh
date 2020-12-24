f37c8362f0afd028c72c194cefe2de5d16ad17ee
Hackwithvyshu <64751167+keralahacker@users.noreply.github.com>
2020-12-24 22:03:55 +0000

    Add files via upload

---
+++ xmas.sh
@@ -0,0 +1,33 @@
+
+
+#!/bin/bash
+
+while true
+do
+	wget -q https://vt100.net/dec/animation/xmas0.txt  -O- | pv -q -L 3600;
+	#sleep 1
+	clear
+	wget -q https://vt100.net/dec/animation/xmas2.txt -O- | pv -q -L 3600;
+	#sleep 1
+	clear
+	wget -q https://vt100.net/dec/animation/xmas3.txt -O- | pv -q -L 3600;
+	#sleep 1
+	clear
+	wget -q https://vt100.net/dec/animation/xmas4.txt -O- | pv -q -L 3600;
+	#sleep 1
+	clear
+	wget -q https://vt100.net/dec/animation/xmas5.txt -O- | pv -q -L 3600;
+	#sleep 1
+	clear
+	wget -q https://vt100.net/dec/animation/xmas6.txt -O- | pv -q -L 3600;
+	#sleep 1
+	clear
+	wget -q https://vt100.net/dec/animation/xmas7.txt -O- | pv -q -L 3600;
+	#sleep 1
+	clear
+	wget -q https://vt100.net/dec/animation/xmas8.txt -O- | pv -q -L 3600;
+	#sleep 1
+	clear
+	wget -q https://vt100.net/dec/animation/xmas9.txt -O- | pv -q -L 3600;
+	#sleep 1
+done
